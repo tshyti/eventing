21c4bd1d202a4c0dcd4cf622f3e06ba25ab20bc1
Taso Shyti <tasoshyti@live.com>
2020-05-16 21:46:28 +0200

    Added basic scaffolding models

---
+++ EventingNetCore/Domain/Entities/Events.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public partial class Events
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ EventingNetCore/Domain/Entities/UserEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public partial class UserEvents
+    {
+        public string Userid { get; set; }
+        public int Eventid { get; set; }
+
+        public virtual Events Event { get; set; }
+        public virtual AspNetUsers User { get; set; }
+    }
+}
