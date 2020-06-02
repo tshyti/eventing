5966f7446969c1d7f1db6134ad4cba2a9e5fbd4e
Taso Shyti <tasoshyti@live.com>
2020-06-02 19:19:22 +0200

    modified app to use next-redux-wrapper

--- eventing-back-office/src/pages/_app.tsx
+++ eventing-back-office/src/pages/_app.tsx
@@ -1,13 +1,10 @@
-import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
 import '../styles/vars.css';
 import '../styles/global.css';
-import store from '../store';
+import { wrapper } from 'store';
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default wrapper.withRedux(MyApp);
--- eventing-back-office/src/store.ts
+++ eventing-back-office/src/store.ts
@@ -1,20 +1,42 @@
+/* eslint-disable no-restricted-syntax */
 import {
   configureStore,
   combineReducers,
   ThunkAction,
   Action,
+  AnyAction,
+  createStore,
 } from '@reduxjs/toolkit';
+import { MakeStore, createWrapper, Context, HYDRATE } from 'next-redux-wrapper';
 import authReducer from 'slices/auth/authSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
-});
-
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
-export default store;
+function reducer(state: RootState, action: AnyAction) {
+  if (action.type === HYDRATE) {
+    const nextState: RootState = {
+      ...state,
+      ...action.payload, // apply delta from hydration
+    };
+
+    // keep client side state
+    // here we should check on what parts of the state are needed
+    // when the page will render from the server side
+    nextState.auth = state.auth;
+    return nextState;
+  }
+  return rootReducer(state, action);
+}
+
+function makeStore(context: Context) {
+  return configureStore({
+    reducer,
+  });
+}
+
+export const wrapper = createWrapper<RootState>(makeStore);
